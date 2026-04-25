49367b97a7e26bc128de31f3b2b61237042b01b4
Ayush Pal <ayush.pal.cs28@tha.edu.in>
2026-04-25 20:47:05 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,98 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import preprocessor as pp
+import helper as h
+import streamlit as st
+
+st.sidebar.title("WhatsApp Chat Analyzer")
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = pp.preprocess(data)
+
+    #fetch unique users
+    user_list = df['user'].unique().tolist()
+    user_list.remove('group_notification')
+    user_list.sort()
+    user_list.insert(0,'overall')
+    selected_usr = st.sidebar.selectbox("show analysis w.r.t", user_list)
+
+    if st.sidebar.button("Show analysis"):
+        st.title("Top statistics")
+        num_messages, num_words, distinct_words, medias, links= h.fetch_data(selected_usr,df)
+        col1, col2,col3,col4,col5 = st.columns(5)
+        with col1:
+            st.metric(f"Total messages",num_messages)
+            # st.header(num_messages)
+        with col2:
+            st.metric(f"Total words",num_words)
+            # st.header(num_words)
+        with col3:
+            st.metric(f"Total distinct words",distinct_words)
+            # st.header(distinct_words)
+        with col4:
+            st.metric(f"Total medias",medias)
+            # st.header(medias)
+        with col5:
+            st.metric(f"Total links",links)
+            # st.title(links)
+        col1,col2 = st.columns(2)
+        with col1:
+            #user timeline
+            st.title("monthly timeline")
+            timeline = h.monthly_timeline(selected_usr,df)
+            fig, ax = plt.subplots()
+            ax.plot(timeline['time'], timeline['message'])
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+        with col2:
+            #activity map
+            st.title('Activity map')
+            busy_day = h.weekly_activity_map(selected_usr,df)
+            fig, ax = plt.subplots()
+            ax.barh(busy_day.index,busy_day.values)
+            st.pyplot(fig)
+
+        #finding the busiest user in the chat
+        if selected_usr == 'overall':
+            st.title("user activity")
+            x,new_df = h.fetch_most_busy_users(df)
+            fig, ax = plt.subplots()
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                ax.bar(x.index,x.values)
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(new_df)
+        col1,col2 = st.columns(2)
+
+        with col1:
+            #WordCloud
+            st.title("Word-Cloud")
+            df_wc = h.create_wordcloud(selected_usr,df)
+            fig, ax = plt.subplots()
+            ax.imshow(df_wc)
+            st.pyplot(fig)
+
+        with col2:
+            #most common words
+            st.title("Top 20 most common words")
+            new_df = h.max_word(selected_usr,df)
+            fig, ax = plt.subplots()
+            ax.barh(new_df['word'],new_df['count'])
+            plt.xticks(rotation='vertical')
+            st.pyplot(fig)
+
+    #activity heatmap:
+    st.title("weekly activity HeatMap")
+    user_heatmap = h.User_heatmap(selected_usr,df)
+    fig, ax = plt.subplots()
+    ax = sns.heatmap(user_heatmap)
+    st.pyplot(fig)
+
+
---
+++ helper.py
@@ -0,0 +1,103 @@
+import nltk
+from urlextract import URLExtract
+from wordcloud import WordCloud
+from collections import Counter
+import pandas as pd
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+#reading the stop words
+with open("/Users/ACS/whatsapp chat analysis/stop_words.txt", "r") as f:
+    content1 = {word.lower() for word in f.read().split()}
+with open("/Users/ACS/whatsapp chat analysis/bengali_pronouns.txt", "r",encoding="utf-8") as f:
+    content2 = {word.lower() for word in f.read().split()}
+nltk_stopwords = set(word.lower() for word in stopwords.words('english'))
+ALL_STOPWORDS = content1.union(content2).union(nltk_stopwords)
+
+extract = URLExtract()
+
+def fetch_data(selected_user, df):
+    #number of messages
+    if selected_user != 'overall':
+        df = df[df['user'] == selected_user]
+    num_messages = df.shape[0]
+    # number of words
+    words = []
+    for message in df['message']:
+        words.extend(message.split())
+    #number of medias
+    medias = df[df['message'] == '<Media omitted>'].shape[0]
+
+    #fetch number of links
+    links = []
+    for message in df['message']:
+        links.extend(extract.find_urls(message))
+    return num_messages, len(words), len(set(words)), medias, len(links)
+def fetch_most_busy_users(df):
+    x = df[df['user'] != 'group_notification']['user'].value_counts()
+    df = round((df[df['user'] != 'group_notification']['user'].value_counts()/df.shape[0])*100,2).reset_index().rename(columns={'user':'name','count':'activity-percentage'})
+    return x,df
+
+#creating the wordcloud
+
+def create_wordcloud(select_user, df):
+    if select_user != 'overall':
+        df = df[df['user'] == select_user]
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+
+    def remove_stopwords(message):
+        words = []
+        for word in message.lower().split():
+            if word.lower() not in ALL_STOPWORDS:
+                words.append(word)
+        return " ".join(words)
+
+    wc = WordCloud(width=500,height=500,min_font_size=7,background_color='white')
+    temp['message'] = temp['message'].apply(remove_stopwords)
+    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+    return df_wc
+
+
+#finding the max words:
+
+def max_word(select_user, df):
+
+    if select_user != 'overall':
+        df = df[df['user'] == select_user]
+    tempdf = df[df['user'] != 'group_notification']
+    tempdf = tempdf[tempdf['message'] != '<Media omitted>\n']
+    words = []
+    for message in tempdf['message']:
+        for word in message.lower().split():
+            if word.lower() not in ALL_STOPWORDS:
+                words.append(word)
+    result = dict(Counter(words))
+    top10 = sorted(result.items(), key=lambda x: x[1], reverse=True)[:20]
+    top10 = pd.DataFrame(top10, columns=['word', 'count'])
+    top10['percentage'] = (top10['count'] / top10['count'].sum() * 100).round(2)
+    return top10
+
+def monthly_timeline(selected_user, df ):
+    if selected_user != 'overall':
+        df = df[df['user'] == selected_user]
+    timeline = df.groupby(['year', 'month_number', 'month']).count()['message'].reset_index()
+    time = []
+    for i in range(timeline.shape[0]):
+        time.append(timeline['month'][i] + "-" + str(timeline['year'][i]))
+    timeline['time'] = time
+    return timeline
+
+
+def weekly_activity_map(selected_user,df):
+    if selected_user != 'overall':
+        df= df[df['user'] == selected_user]
+    return df['day_name'].value_counts()
+
+
+def User_heatmap(selected_user,df):
+    if selected_user != 'overall':
+        df = df[df['user']== selected_user]
+    User_heatmap = df.pivot_table(index='day_name', columns='period', values='message',aggfunc='count').fillna(0)
+    return User_heatmap
